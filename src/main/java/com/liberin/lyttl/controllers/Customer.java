b5ef167a61998a9d925925ee41186e34c9c96a4e
himanshu.him.shar <himanshu.sharma@liberintechnologies.com>
2024-04-15 15:12:37 +0530

    Keycloak Integration done

---
+++ src/main/java/com/liberin/lyttl/controllers/Customer.java
@@ -0,0 +1,8 @@
+package com.liberin.lyttl.controllers;
+
+public class Customer {
+    private String name;
+    private String address;
+
+
+}
--- src/main/java/com/liberin/lyttl/controllers/MyController.java
+++ src/main/java/com/liberin/lyttl/controllers/MyController.java
@@ -1,15 +1,18 @@
 package com.liberin.lyttl.controllers;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class MyController {
 
-    @GetMapping("/getmessage")
-    @PreAuthorize("hasRole('')")
-    public String getMessage(){
-        return "Hello, I am your Dashboard Page";
-    }
+    @GetMapping("/hello")
+   public String getMessage(){
+       return "Hi, I am getting Accessed";
+   }
+
 }
